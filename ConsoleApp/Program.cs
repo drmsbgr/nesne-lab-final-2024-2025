f162bf86f20db62e68267f3be3b8777ed52d18db
drmsbgr <drmsbgr@gmail.com>
2025-01-15 17:01:02 +0300

    Employees Added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using ConsoleApp.Entities;
 using ConsoleApp.Repositories;
+using ConsoleApp.Services;
 
 var context = new RepositoryContext();
 
@@ -12,6 +13,19 @@ var deps = new List<Department>()
 };
 
 AddDepartments(context, deps);
+
+var emps = new List<Employee>()
+{
+new () {EmployeeId = 1, FullName = "John Doe", Salary = 1000,DepartmentId = 1},
+new () {EmployeeId = 2, FullName = "Jane Smith", Salary = 2000,DepartmentId = 2},
+new () {EmployeeId = 3, FullName = "Alice Johnson", Salary = 3000,DepartmentId = 1},
+new () {EmployeeId = 4, FullName = "Bob Williams", Salary = 2500,DepartmentId = 3},
+new () {EmployeeId = 5, FullName = "Charlie Brown", Salary = 4000,DepartmentId = 2}
+};
+
+var empRepo = new EmployeeRepository(context);
+var empService = new EmployeeService(empRepo);
+emps.ForEach(e => empService.AddEmployee(e));
 
 
 static void AddDepartments(RepositoryContext context, ICollection<Department> collection)
